39ed1219ac0bb9d671ae5068cd39ee15adfca922
ktoshiya <k.toshiya.1107@gmail.com>
2021-03-18 13:45:01 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"net/http"
+)
+
+func main() {
+    engine:= gin.Default()
+	ua := ""
+
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
+    engine.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "hello world",
+			"User-Agent": ua,
+        })
+    })
+    engine.Run(":3000")
+}
